871d2156c915a0bc704ac3c250c3bd299e8388ff
Nikolaj Nøhr-Rasmussen <Nikolaj (lokal)@N>
2020-01-01 17:59:09 +0100

    n/a

--- Arduino/AutoIrrigation/__vm/.AutoIrrigation.vsarduino.h
+++ Arduino/AutoIrrigation/__vm/.AutoIrrigation.vsarduino.h
@@ -18,8 +18,11 @@
 #define __ets__
 #define ICACHE_FLASH
 #define NONOSDK221 1
-#define F_CPU {build.f_cpu}
+#define F_CPU 80000000L
 #define LWIP_OPEN_SRC
+#define TCP_MSS 536
+#define LWIP_FEATURES 1
+#define LWIP_IPV6 0
 #define ARDUINO 10809
 #define ARDUINO_ESP8266_WEMOS_D1MINI
 #define ARDUINO_ARCH_ESP8266
